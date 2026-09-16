6cc1a7b1a51c61dfbb63ddd1ec5b257254b79e7a
sagar patidar <152028847+Sagarpatidar407@users.noreply.github.com>
2026-09-16 19:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (47.37%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,72 +1,25 @@
 class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int n1 = m-1;
+        int n2 = n-1;
+        int k = m+n-1;
 
-    public void merge(int[] arr, int left, int mid, int right){
+        while(n2 >= 0){
 
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        int[] leftarr = new int[n1];
-        int[] rightarr = new int[n2];
-
-        int k = left;
-
-        for(int i=0;i<n1;i++){
-            leftarr[i] = arr[k++];
-        }
-
-        k = mid+1;
-
-        for(int i=0;i<n2;i++){
-            rightarr[i] = arr[k++];
-        }
-
-        int leftindex = 0, rightindex = 0, mainindex = left;
-
-        while(leftindex < n1 && rightindex < n2){
-
-            if(leftarr[leftindex] < rightarr[rightindex]){
-                arr[mainindex++] = leftarr[leftindex++];
+            if(n1>=0 && nums1[n1] > nums2[n2]){
+                nums1[k] = nums1[n1];
+                n1--;
             }else{
-                arr[mainindex++] = rightarr[rightindex++];
+                nums1[k] = nums2[n2];
+                n2--;
             }
 
-        }
-
-        while(leftindex < n1){
-            arr[mainindex++] = leftarr[leftindex++];
-        }
-
-        while(rightindex < n2){
-            arr[mainindex++] = rightarr[rightindex++];
-        }
-
-
-
-
-    }
-
-
-    public void mergesort(int[] nums, int s, int e){
-
-        if(s<e){
-            int mid = s + (e-s)/2;
-
-            mergesort(nums, s, mid);
-            mergesort(nums, mid+1, e);
-
-            merge(nums, s, mid, e);
+            k--;
 
         }
 
-    }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int j=0;
-        for(int i=m;i<n+m;i++){
-            nums1[i] = nums2[j++];
-        }
-
-        mergesort(nums1, 0, n+m-1);
     }
 }
